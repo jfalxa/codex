56d26abadaea080fcc44134e1af5334f96dfa86a
jfalxa <jfalxa.sta@gmail.com>
2017-05-08 01:30:21 +0200

    [client] Styled search input

--- client/src/components/search/SearchInput.js
+++ client/src/components/search/SearchInput.js
@@ -1,9 +1,21 @@
 import styled from 'styled-components';
 
+import { lightBackground } from '../../style/theme';
+
 
 const SearchInput = styled.input`
 
     flex: 1;
+
+    border: 0;
+    padding: 5px 10px;
+
+    background-color: ${ lightBackground };
+
+    font-size: 1.5em;
+    font-weight: bold;
+
+    outline: 0;
 
 `;
 
--- client/src/style/theme.js
+++ client/src/style/theme.js
@@ -1,2 +1,4 @@
 export const bodyFont  = '"Roboto Slab", serif';
 export const titleFont = 'Roboto, sans-serif';
+
+export const lightBackground = '#F5F5F5';
