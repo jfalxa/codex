689dee501438d722d87b692d7bc4ef9637264764
jfalxa <jfalxa.sta@gmail.com>
2017-05-31 16:07:06 +0200

    [client] Fixed autocomplete cycling

--- client/src/utils/circleMotion.js
+++ client/src/utils/circleMotion.js
@@ -1,4 +1,10 @@
 export default function circleMotion( position, size, movement )
 {
+    // when nothing is highlighted and the up key is pressed, select the last el
+    if ( position === -1 && movement === -1 )
+    {
+        return size - 1;
+    }
+
     return ( position + movement + size ) % size;
 }
