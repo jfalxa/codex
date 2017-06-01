d07bd95fa175a4ef86aa9359a0cbedc263320ac6
jfalxa <jfalxa.sta@gmail.com>
2017-06-01 21:17:45 +0200

    [server] Fixed autocomplete not reading the right query param

--- server/src/controllers/autocomplete.js
+++ server/src/controllers/autocomplete.js
@@ -4,15 +4,15 @@ const Tag   = require( '../models/tag' );
 
 exports.autocompleteLabel = function autocompleteLabel( req, res, next )
 {
-    Label.findLabel( req.query.value )
-        .then( tags => res.status( 200 ).json( tags ) )
+    Label.findLabel( req.query.fragment )
+        .then( labels => res.status( 200 ).json( labels ) )
         .catch( error => res.status( 500 ).json( { error } ) );
 };
 
 
 exports.autocompleteTag = function autocompleteTag( req, res, next )
 {
-    Tag.findTag( req.query.value )
+    Tag.findTag( req.query.fragment )
         .then( tags => res.status( 200 ).json( tags ) )
         .catch( error => res.status( 500 ).json( { error } ) );
 };
